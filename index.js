b529ed586c7bb8449686246061481f20a1b7042f
Tshweu <t.t.sephiri@gmail.com>
2020-10-08 09:43:48 +0200

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 const bodyParser = require('body-parser');
 
 const transporter = nodemailer.createTransport({
